1b180fdcea21fe6a35c01aaa9736324144858668
Bradford Foxworth-Hill <Bradford.foxworthhill@gmail.com>
2019-06-18 18:35:52 -0400

    Added Polly Resilience

---
+++ src/EfMicroservice.Core/Api/Configuration/HttpClient/BulkheadConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace EfMicroservice.Core.Api.Configuration.HttpClient
+{
+    public class BulkheadConfiguration
+    {
+        public int MaxParallelization { get; set; }
+
+        public int MaxQueuingActions { get; set; }
+    }
+}
---
+++ src/EfMicroservice.Core/Api/Configuration/HttpClient/RetryRequestConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfMicroservice.Core.Api.Configuration.HttpClient
+{
+    public class RetryRequestConfiguration
+    {
+        public IEnumerable<int> IntervalsMs { get; set; }
+
+        public IEnumerable<string> HttpStatusCodes { get; set; }
+    }
+}
